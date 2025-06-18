d7a6bd41c52c00b4eded2243a7546dabb8520db6
TranQuangTruc <tranquangtruc19@gmail.com>
2025-06-18 15:34:04 +0700

    Thêm chức năng Đăng nhập và các trường hợp thông tin đăng ký sai

---
+++ hivapp(Login)/src/main/java/com/example/hivapp/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.hivapp.repository;
+
+import com.example.hivapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByPhone(String phone);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
---
+++ hivapp(Login)/src/test/java/com/example/hivapp/HivappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hivapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HivappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
